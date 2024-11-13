a94c8b91a063a56df6f4faaf4a2f7524b0a2dfb8
Coen A Evans <55774851+oneping-1@users.noreply.github.com>
2024-11-13 09:17:22 -0500

    draw_inning_arrows
    Another graphic that used to be a font

--- on_deck2/display_manager.py
+++ on_deck2/display_manager.py
@@ -4,9 +4,15 @@ It is used to draw lines, text, and clear sections of the display.
 """
 
 import platform
-from on_deck.colors import Colors
 import time
 import math
+
+if __name__ == '__main__':
+    # should probably do this correctly
+    # will fix at some point :)
+    from colors import Colors
+else:
+    from on_deck.colors import Colors
 
 if platform.system() == 'Windows':
     from RGBMatrixEmulator import RGBMatrix, RGBMatrixOptions, graphics # pylint: disable=E0401
@@ -88,10 +94,14 @@ class DisplayManager:
             self.draw_pixel(x, y, color) # Not included in the loops
 
         for degrees in range(0, 91, 10):
-            # Dont need to go all the way around
-            # because of rotational symmetry
+            # Used to get rotational symmetry
             for r in range(start, stop, -1):
+                # Used to prevent .5 (sin30 or cos60)
+                # to get rounded to nearest even number
+                # Want to avoid any errors with things getting rounded
+                # up in one direction but down in the other
                 r_eff = r - .01
+
                 d1 = degrees
                 d2 = d1 + 90
                 d3 = d2 + 90
@@ -102,30 +112,21 @@ class DisplayManager:
                 a3 = math.radians(d3)
                 a4 = math.radians(d4)
 
-                x1 = x + r_eff * math.cos(a1)
-                y1 = y + r_eff * math.sin(a1)
-                x1 = round(x1)
-                y1 = round(y1)
+                x1 = round(x + r_eff * math.cos(a1))
+                y1 = round(y + r_eff * math.sin(a1))
                 self.draw_pixel(x1, y1, color)
 
-                x2 = x + r_eff * math.cos(a2)
-                y2 = y + r_eff * math.sin(a2)
-                x2 = round(x2)
-                y2 = round(y2)
+                x2 = round(x + r_eff * math.cos(a2))
+                y2 = round(y + r_eff * math.sin(a2))
                 self.draw_pixel(x2, y2, color)
 
-                x3 = x + r_eff * math.cos(a3)
-                y3 = y + r_eff * math.sin(a3)
-                x3 = round(x3)
-                y3 = round(y3)
+                x3 = round(x + r_eff * math.cos(a3))
+                y3 = round(y + r_eff * math.sin(a3))
                 self.draw_pixel(x3, y3, color)
 
-                x4 = x + r_eff * math.cos(a4)
-                y4 = y + r_eff * math.sin(a4)
-                x4 = round(x4)
-                y4 = round(y4)
+                x4 = round(x + r_eff * math.cos(a4))
+                y4 = round(y + r_eff * math.sin(a4))
                 self.draw_pixel(x4, y4, color)
-
 
     def draw_diamond(self, x: int, y: int, radius: int, thickness: int,
         fill: bool, color: graphics.Color):
@@ -143,27 +144,52 @@ class DisplayManager:
 
         start = radius
         stop = radius - thickness
+
         if fill:
             stop = 0
             self.draw_pixel(x, y, color) # Not included in the loop
 
-        for i in range(start, stop, -1):
+        for layer in range(start, stop, -1):
             x1 = x
-            y1 = y - i
+            y1 = y - layer
 
-            x2 = x + i
+            x2 = x + layer
             y2 = y
 
             x3 = x
-            y3 = y + i
+            y3 = y + layer
 
-            x4 = x - i
+            x4 = x - layer
             y4 = y
 
             self.draw_line(x1, y1, x2, y2, color)
             self.draw_line(x2, y2, x3, y3, color)
             self.draw_line(x3, y3, x4, y4, color)
             self.draw_line(x4, y4, x1, y1, color)
+
+    def draw_inning_arrow(self, x: int, y: int, height: int, up: bool,
+        color: graphics.Color):
+        """
+        This method is used to draw an arrow on the display.
+        The x and y coordinates are the center of the base of the arrow.
+        The arrow will be filled in.
+
+        Args:
+            x (int): X coordinate of the center of the base of the arrow
+            y (int): Y coordinate of the center of the base of the arrow
+            height (int): Height of the arrow
+            up (bool): Whether or not the arrow is pointing up (is_top_inning)
+            color (graphics.Color): Color of the arrow
+        """
+        dy = -1
+        if up:
+            dy = 1
+
+        for i in range(height):
+            x1 = x - i
+            x2 = x + i
+            y1 = y + (height - i - 1) * dy
+            self.draw_line(x1, y1, x2, y1, color)
 
     def clear_section(self, x1, y1, x2, y2):
         """This method is used to clear a section of the display."""
@@ -178,19 +204,30 @@ class DisplayManager:
             graphics.DrawLine(self.canvas, x1, y1 + i, x2, y1 + i, color)
 
 if __name__ == '__main__':
-    from colors import Colors
-
     display = DisplayManager(get_options())
     R = 5
     display.draw_circle(10, 10, R, 1, False, Colors.red)
     display.draw_circle(30, 10, R, 2, False, Colors.green)
     display.draw_circle(50, 10, R, 3, False, Colors.light_blue)
     display.draw_circle(70, 10, R, 1, True, Colors.yellow)
+    display.draw_pixel(10, 10, Colors.white)
+    display.draw_pixel(30, 10, Colors.white)
+    display.draw_pixel(50, 10, Colors.white)
+    display.draw_pixel(70, 10, Colors.white)
 
     display.draw_diamond(10, 30, R, 1, False, Colors.red)
     display.draw_diamond(30, 30, R, 2, False, Colors.green)
     display.draw_diamond(50, 30, R, 3, False, Colors.light_blue)
     display.draw_diamond(70, 30, R, 1, True, Colors.yellow)
+    display.draw_pixel(10, 30, Colors.white)
+    display.draw_pixel(30, 30, Colors.white)
+    display.draw_pixel(50, 30, Colors.white)
+    display.draw_pixel(70, 30, Colors.white)
+
+    display.draw_inning_arrow(10, 50, R, True, Colors.red)
+    display.draw_inning_arrow(30, 50, R, False, Colors.green)
+    display.draw_pixel(10, 50, Colors.white)
+    display.draw_pixel(30, 50, Colors.white)
     display.swap_frame()
 
     while True:
