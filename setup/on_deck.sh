c42dc01ebff28624796af175cdc0fc22f5adbd70
Coen A Evans <coen.evans@gmail.com>
2025-03-15 17:44:10 -0400

    output name changes

--- setup/on_deck.sh
+++ setup/on_deck.sh
@@ -9,6 +9,6 @@ cd /home/on_deck/on_deck || exit
 git fetch
 git pull
 
-/home/on_deck/venv/bin/python /home/on_deck/on_deck/on_deck/on_deck_fetcher.py >> /home/ondeck/on_deck_fetcher.log &
-/home/on_deck/venv/bin/gunicorn -w 2 -b 0.0.0.0:80 on_deck_server:app >> /home/ondeck/on_deck_server.log &
-sudo python /home/on_deck/on_deck/on_deck/on_deck_display.py >> /home/ondeck/on_deck_display.log &
+/home/on_deck/venv/bin/python /home/on_deck/on_deck/on_deck/on_deck_fetcher.py >> /home/on_deck/on_deck_fetcher.log &
+/home/on_deck/venv/bin/gunicorn -w 2 -b 0.0.0.0:80 on_deck_server:app >> /home/on_deck/on_deck_server.log &
+sudo python /home/on_deck/on_deck/on_deck/on_deck_display.py >> /home/on_deck/on_deck_display.log &
