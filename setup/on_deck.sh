b7638140712a75b91c9e5a456e167cc2d3ee7a65
Coen A Evans <coen.evans@gmail.com>
2025-03-15 17:38:04 -0400

    sh fixes

--- setup/on_deck.sh
+++ setup/on_deck.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-exec >> /home/ondeck/ondeck.log 2>&1
+exec >> /home/on_deck/ondeck.log 2>&1
 
-cd /home/ondeck/at_bat || exit
+cd /home/on_deck/at_bat || exit
 git fetch
 git pull
 
-cd /home/ondeck/OnDeck-RaspberryPi || exit
+cd /home/on_deck/on_deck || exit
 git fetch
 git pull
 
-/home/ondeck/venv/bin/python /home/ondeck/OnDeck-RaspberryPi/on_deck/on_deck_fetcher.py >> /home/ondeck/on_deck_fetcher.log &
-/home/ondeck/venv/bin/gunicorn -w 2 -b 0.0.0.0:80 on_deck_server:app >> /home/ondeck/on_deck_server.log &
-sudo python /home/ondeck/OnDeck-RaspberryPi/on_deck/on_deck_display.py >> /home/ondeck/on_deck_display.log &
+/home/on_deck/venv/bin/python /home/on_deck/on_deck/on_deck/on_deck_fetcher.py >> /home/ondeck/on_deck_fetcher.log &
+/home/on_deck/venv/bin/gunicorn -w 2 -b 0.0.0.0:80 on_deck_server:app >> /home/ondeck/on_deck_server.log &
+sudo python /home/on_deck/on_deck/on_deck/on_deck_display.py >> /home/ondeck/on_deck_display.log &
--- setup/on_desk.sh
+++ setup/on_desk.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 exec >> /home/ondeck/ondeck.log 2>&1
 
-cd /home/ondeck/at_bat || exit
+cd /home/on_deck/at_bat || exit
 git fetch
 git pull
 
-cd /home/ondeck/OnDeck-RaspberryPi || exit
+cd /home/on_deck/on_deck || exit
 git fetch
 git pull
 
-/home/ondeck/new/bin/python /home/ondeck/OnDeck-RaspberryPi/on_desk.py
+/home/ondeck/new/bin/python /home/ondeck/on_deck/on_deck/on_desk.py
