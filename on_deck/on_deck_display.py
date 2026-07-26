78b50c17be3243fb67ad74e02dcce59301383003
oneping-1 <coen.evans@gmail.com>
2026-07-26 16:26:01 -0400

    Refactor GamecastHandler to streamline mode handling and improve gamecast display updates

--- on_deck/on_deck_display.py
+++ on_deck/on_deck_display.py
@@ -232,10 +232,14 @@ class GamecastHandler:
             brightness = int(message['data'])
             self.display_manager.set_brightness(brightness_dict[brightness])
 
-        # mode = self.redis.get('mode')
-        # if channel == b'mode':
-        #     mode = message['data']
+        mode = self.redis.get('mode')
+        if channel == b'mode':
+            mode = message['data']
 
+        if mode == b'gamecast':
+            self.display_manager.clear_section(129, 0, 384, 256)
+            self.gamecast.print_game(self.gamecast_game)
+            print('gamecast reloaded')
 
     def update_gamecast(self) -> Union[bool, dict]:
         """
@@ -263,19 +267,13 @@ class GamecastHandler:
             print('changing settings')
             self.change_settings(message)
             return False
-        
+
         new_data = json.loads(message['data'])
         # print(f'{new_data=}\n')
         if new_data == {}:
             return False
 
         self.gamecast_game = recursive_update(self.gamecast_game, new_data)
-            
-        mode = self.redis.get('mode')
-        if mode == b'gamecast':
-            self.display_manager.clear_section(129, 0, 384, 256)
-            self.gamecast.print_game(self.gamecast_game)
-            print('gamecast reloaded')
         return new_data
 
     def print_gamecast_game(self) -> bool:
