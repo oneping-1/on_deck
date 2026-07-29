b669b84b069f7ccf60d414e8842a93ad945fe2f6
oneping-1 <coen.evans@gmail.com>
2026-07-29 18:46:47 -0400

    Add shortcut URL for settings and improve gamecast ID handling

--- on_deck/on_deck_server.py
+++ on_deck/on_deck_server.py
@@ -31,6 +31,7 @@ class Server:
         self.app.add_url_rule('/', 'home', self.home, methods=['GET'])
         self.app.add_url_rule('/reboot', 'reboot', self.reboot, methods=['GET'])
         self.app.add_url_rule('/settings', 'settings', self.settings, methods=['GET'])
+        self.app.add_url_rule('/s', 's', self.settings, methods=['GET'])
         self.app.add_url_rule('/<int:gamepk>', 'gamepk', self.gamepk, methods=['GET'])
         self.app.add_url_rule('/gamecast', 'gamecast', self.gamecast, methods=['GET'])
 
@@ -138,15 +139,15 @@ class Server:
         delay_short = request.args.get('d', default=None)
         brightness = request.args.get('brightness', default=None)
         brightness_short = request.args.get('b', default=None)
-        gamecast_id_short = request.args.get('id', default=None)
         gamecast_id = request.args.get('gamecast_id', default=None)
-            
+        gamecast_id_short = request.args.get('id', default=None)
+
         if delay_short is not None and delay is None:
             delay = delay_short
-            
+
         if brightness_short is not None and brightness is None:
             brightness_short = brightness
-        
+
         if gamecast_id_short is not None and gamecast_id is None:
             gamecast_id = gamecast_id_short
 
